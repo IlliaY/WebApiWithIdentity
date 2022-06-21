237bb0db1a0d671ede2a2c29568dd8b49318bfb1
Illia Yevtushevskyi <illia.yevtushevskyi@gmail.com>
2022-06-21 02:56:12 +0200

    Added user input validation

--- WebApi.BLL/Models/UserLoginModel.cs
+++ WebApi.BLL/Models/UserLoginModel.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApi.BLL.Models
 {
     public class UserLoginModel
     {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(20)]
         public string UserName { get; set; }
+
+        [Required]
         public string Password { get; set; }
     }
 }
--- WebApi.BLL/Models/UserRegisterModel.cs
+++ WebApi.BLL/Models/UserRegisterModel.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApi.BLL.Models
 {
     public class UserRegisterModel
     {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(20)]
         public string UserName { get; set; }
+
+        [Required]
         public string Password { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", ErrorMessage = "Invalid email format")]
         public string Email { get; set; }
     }
 }
